4b221d3077aa0b4e48ef3c66be886cefc4142315
ArtiomB5 <74674499+ArtiomB5@users.noreply.github.com>
2021-05-24 11:45:31 +0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,64 @@
+let inputButton = document.getElementById('inputButton');
+inputButton.addEventListener('click', inputDataCheck);
+//получения элемента кнопки и вызов функции при клике на кнопку
+
+let vowelsList = ['a', 'o', 'e', 'i', 'y', 'u'];
+//список гласных
+let consonantYList = ['ya', 'yo', 'ye', 'yi', 'yu'];
+//список когда y согласный
+
+let vowels = 0;
+
+//функция обработки введенных данных, запускается по нажатию кнопки
+function inputDataCheck() {
+    let InputWord = document.getElementById('inputField').value;
+    //получение данных полей названия, даты, суммы
+
+    if (InputWord === "") {
+        alert('Please input word.')
+    } else {
+        vowelsCounter(InputWord);
+    }
+    //проверка поля вводы на наличие текста, если не пуста то вызывается функция подсчета гласных
+}
+
+//функция подсчета гласных, в качестве параметра получает введенный пользователем текст
+function vowelsCounter(InputWord) {
+    //перебор массива гласных звуков и подсчет их кол-ва
+    vowelsList.forEach(
+        function (item) {
+            for (let index = 0; index < InputWord.length; index++) {
+                if (item === InputWord[index]) {
+                    vowels++;
+                }
+            }
+        }
+    )
+    
+    
+    //перебор массива согласных случаев y и подсчет их кол-ва
+    let pos = 0;
+    let consonants = 0;
+    consonantYList.forEach(
+        function (item) {
+            while (true) {
+                let foundPos = InputWord.indexOf(item, pos);
+                if (foundPos == -1) break;
+
+                consonants++;
+                pos = foundPos + 1;
+            }
+        }
+    )
+    
+    //сообщение пользовател. количества гласных звуков
+    alert('There are ' + (vowels - consonants) + ' vowels!');
+    vowels = 0;
+}
+
+//функция очистки поля ввода
+function inputClear() {
+    document.getElementById('inputField').value = '';
+}
+
+
